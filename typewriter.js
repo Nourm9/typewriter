184906019575e2e968c8263fb1d5966b24f8f1ae
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-12-12 00:26:27 +0000

    update: typewriter.js; print on new line

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,9 @@ const sentence = "hello there from lighthouse labs";
 let interval = 0;
 for (const char of sentence) {
   setTimeout(() => {
-    process.stdout.write(char);
+    //prints out each character of string with 50ms delay
+    //prints each char on new line
+    process.stdout.write(char+"\n");
   }, interval);
   interval += 50;
 };
